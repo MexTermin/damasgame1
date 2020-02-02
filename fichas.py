c0b02e39512eaa7f80f9bba6dfa861335421a47b
yael <yaeldemil01@hotmail.com>
2020-02-02 12:05:27 -0400

    Simplificacion y arreglos de errores

--- fichas.py
+++ fichas.py
@@ -5,20 +5,19 @@ class movinvalido(Exception):
     pass
 
 def fichamove(ficha,tomove,interfaz,player):
-
+    xpos = int(ficha[0])
+    ypos = int( convert(ficha[2].lower()) )
     if player == "n":
-        xpos = int(ficha[0])
-        ypos = int( convert(ficha[2].lower()) )
         if interfaz[xpos][ypos] != "n":
             raise fichaInvalida("Invalid tab")
         if tomove.upper() == "L":
-            if vlibre((xpos,ypos),interfaz) == "left" or  vlibre((xpos,ypos),interfaz) == "bot":
+            if vlibre((xpos,ypos),interfaz,player) == "left" or  vlibre((xpos,ypos),interfaz,player) == "bot":
                 xnew = xpos-1
                 ynew = ypos-1 
             else:
                 raise movinvalido("Esta ficha no se puede mover al left")
         elif tomove.upper() == "R":
-            if vlibre((xpos,ypos),interfaz) == "right" or vlibre((xpos,ypos),interfaz) == "bot":
+            if vlibre((xpos,ypos),interfaz,player) == "right" or vlibre((xpos,ypos),interfaz,player) == "bot":
                 xnew = xpos-1
                 ynew = ypos+1
             else:
@@ -30,21 +29,25 @@ def fichamove(ficha,tomove,interfaz,player):
         player="b"
 
     elif player == "b":
-        xpos = int(ficha[0])
-        ypos = int( convert(ficha[2].lower()) )
-        if interfaz[xpos][ypos] != "b":
-            raise fichaInvalida("Invalid tab")
-        if tomove.upper() == "L":
-            xnew = xpos+1
-            ynew = ypos-1 
-        elif tomove.upper() == "R":
-            xnew = xpos+1
-            ynew = ypos+1
-        else:
-            return "Invalid move"
-        interfaz[xpos][ypos] = "."
-        interfaz[xnew][ynew] = "b"
-        player="n"
+            if interfaz[xpos][ypos] != "b":
+                raise fichaInvalida("Invalid tab")
+            if tomove.upper() == "L":
+                if vlibre((xpos,ypos),interfaz,player) == "left" or  vlibre((xpos,ypos),interfaz,player) == "bot":
+                    xnew = xpos+1
+                    ynew = ypos-1 
+                else:
+                    raise movinvalido("Esta ficha no se puede mover al left")
+            elif tomove.upper() == "R":
+                if vlibre((xpos,ypos),interfaz,player) == "right" or vlibre((xpos,ypos),interfaz,player) == "bot":
+                    xnew = xpos+1
+                    ynew = ypos+1
+                else:
+                    raise movinvalido("Esta ficha no se puede mover al right")
+            else:
+                return "Invalid move"
+            interfaz[xpos][ypos] = "."
+            interfaz[xnew][ynew] = "b"
+            player="n"
     return interfaz,player
     
 
@@ -59,28 +62,35 @@ def convert(string):
             "f":6,
             "g":7,
             "h":8,
-            
-            #  1:"a",
-            #  2:"b",
-            #  3:"c",
-            #  4:"d",
-            #  5:"e",
-            #  6:"f",
-            #  7:"g",
-            #  8:"h"
+
     }
 
     return switcher[string]
 
-def vlibre(ficha,interfaz):
+def vlibre(ficha,interfaz, player):
     left=False
     right=False
-    if interfaz[ficha[0]][ficha[1]] =="n":
-        if interfaz[ficha[0]-1][ficha[1]-1] != "n" and interfaz[ficha[0]-1][ficha[1]-1] != "b":
-            left = True
-
-        if interfaz[ficha[0]-1][ficha[1]+1] != "n" and interfaz[ficha[0]-1][ficha[1]+1] != "b":
-            right = True
+    # asignamos los valores que comparan si esta libre el camino segun sean para las fichas N o B
+    if player == "n":
+        # validamos si la ficha elegida pertenece al jugador
+        if interfaz[ficha[0]] [ficha[1]] =="n":
+            x1,y1,x2,y2=-1,-1,-1,1
+        else:
+            return False
+    if player == "b":
+        #--------------------------------------------------
+        if interfaz[ficha[0]] [ficha[1]] =="b":
+            x1,y1,x2,y2=1,-1,1,1
+        else:
+            return False       
+    #-------------------------------------------------------------------------------------------- 
+    #  Revisamos si la ficha tiene libre la derecha y la izquierda
+    if interfaz[ficha[0]+x1] [ficha[1]+y1] != "n" and interfaz[ficha[0]+x1] [ficha[1]+y1] != "b":
+        left = True
+    if interfaz[ficha[0]+x2] [ficha[1]+y2] != "n" and interfaz[ficha[0]+x2] [ficha[1]+y2] != "b":
+        right = True
+    #-------------------------------------------------------------------------------------------
+    #Segun los resultados de las pruebas anteriores regresamos cual espacio tiene libre la ficha
     if left == True and right == True:
         return "bot"
     elif left == True and right == False:
@@ -89,13 +99,6 @@ def vlibre(ficha,interfaz):
         return "right"
     else:
         return False
-
-    # for first in range(0, len(fichas["n"])  ):
-    #     for second in range(len(fichas["b"])):
-    #         if fichas["n"][first][0][0]-1 != fichas["b"][second][0][0]  and  fichas["n"][first][0][1]-1 != fichas["b"][second][0][1]:
-    #             fichas["n"][first][1]=True       
-    #return  fichas   #["n"][i][0][0]-1,  fichas["n"][i][0][1]-1
-    
 
 
 def poss(lista):
